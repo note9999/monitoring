7b5b44384bd861209c892498b2a973db666fc70b
note9999 <sanghoon273@gmail.com>
2024-01-06 16:36:33 +0900

    Controller, Dto 구현

--- src/main/java/com/example/searchbasic/SearchController.java
+++ src/main/java/com/example/searchbasic/SearchController.java
@@ -1,4 +1,22 @@
 package com.example.searchbasic;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SearchController {
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService){
+
+        this.searchService = searchService;
+    }
+
+    @PostMapping("/search")
+    public SearchKeywordDto search(String keyword) {
+//        return searchService.save(keyword);
+        return null;
+    }
+
 }
--- src/main/java/com/example/searchbasic/SearchKeywordDto.java
+++ src/main/java/com/example/searchbasic/SearchKeywordDto.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 
 @Getter
 public class SearchKeywordDto {
-
     private final String keyword;
 
-    private final Long serachCnt;
+    private final Long searchCnt;
 
-    public SearchKeywordDto(SearchKeyword)
+    public SearchKeywordDto(SearchKeyword searchKeyword) {
+        keyword = searchKeyword.getKeyword();
+        searchCnt = searchKeyword.getSearchCnt();
+    }
 }
