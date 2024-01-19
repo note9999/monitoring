5e6315bf189963d3b434b8485190054cdf3cffbd
note9999 <sanghoon273@gmail.com>
2024-01-19 21:33:53 +0900

    ngrinder와 cloudwatch와 사투 중..

--- src/main/java/com/example/searchbasic/SearchController.java
+++ src/main/java/com/example/searchbasic/SearchController.java
@@ -1,9 +1,13 @@
 package com.example.searchbasic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SearchController {
     private final SearchService searchService;
 
@@ -12,7 +16,8 @@ public class SearchController {
     }
 
     @PostMapping("/search")
-    public SearchKeywordDto search(String keyword) {
+    public SearchKeywordDto search(@RequestBody String keyword) {
+        log.error("[SearchController.search] keyword = {}", keyword);
         return searchService.save(keyword);
     }
 
